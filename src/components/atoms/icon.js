2867ab05d1c47d320a851bc761a0c1601cfd4554
adam-metastay <adam.shoaib@metastay.com>
2021-06-23 19:14:31 +0530

    changes

--- src/components/atoms/icon.js
+++ src/components/atoms/icon.js
@@ -1,42 +1,40 @@
 import React from 'react';
-import {View,Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import Color from '../../constants/color';
 
 const Icon = props => {
-    
-    return(
-        <View style={styles.iconContainer} >
-            <View style={styles.iconUnderText} >
+    return (
+        <View style={styles.iconUnderText} >
             <View style={styles.icon} >
                 <Text style={styles.iconTextInside} >{props.textOnIcon}</Text>
             </View>
-                <Text>{props.fullname}</Text> 
+            <View>
+                <Text>{props.fullname}</Text>
             </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    iconUnderText:{
-        flexDirection:'column',
-        justifyContent:'space-around'
+    iconUnderText: {
+        backgroundColor: 'red'
     },
-    iconContainer:{
-        flexDirection:'row',
-        justifyContent:'space-evenly',
-        backgroundColor:"#465bb3"
+    iconContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: "#465bb3"
     },
-    icon:{
-        width:80,
-        height:80,
-        backgroundColor:Color.accent,
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:40
+    icon: {
+        width: 80,
+        height: 80,
+        backgroundColor: Color.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 40
     },
-    iconTextInside:{
-        fontSize:40
+    iconTextInside: {
+        fontSize: 40
     }
 })
 
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {View, StyleSheet,FlatList} from 'react-native';
-import {useSelector} from 'react-redux';
+import { View, StyleSheet, FlatList, SafeAreaView } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import SubHeading from '../components/atoms/SubHeading';
 // import IconAndName from '../components/molecules/IconAndName';
@@ -8,46 +8,49 @@ import Color from '../constants/color';
 import Icon from '../components/atoms/icon';
 import IconAndName from '../components/molecules/iconAndName';
 
-const Dashboard = () =>{
-    const users = useSelector(state=>state.user.availableUsers);
-    return(
-        <View style={styles.container} >
-            <SubHeading title='Favourite Contacts'/>
-            <FlatList 
-                data={users}
-                keyExtractor={item => item.userid}
-                renderItem={itemData =>(
-                    <Icon 
-                        textOnIcon={itemData.item.usershortname}
-                        fullname={itemData.item.username}
-                    />
-                )
-                }
-            />
-            
-            {/* <IconAndName /> */}
-        </View>
+const Dashboard = () => {
+    const users = useSelector(state => state.user.availableUsers);
+    return (
+        <SafeAreaView style={styles.safeAreaViewcontainer}>
+                <SubHeading title='Favourite Contacts' />
+                <FlatList
+                    horizontal
+                    data={users}
+                    keyExtractor={item => item.userid}
+                    renderItem={itemData => (
+                        <Icon
+                            textOnIcon={itemData.item.usershortname}
+                            fullname={itemData.item.username}
+                        />
+                    )
+                    }
+                />
+        </SafeAreaView>
     );
 }
 Dashboard.navigationOptions = navbar => {
-    return{
-        headerTitle:"Inbox",
-        headerStyle:{
-            backgroundColor:Color.primary,
-            height:60
+    return {
+        headerTitle: "Inbox",
+        headerStyle: {
+            backgroundColor: Color.primary,
+            height: 60
         },
-        headerTintColor:Color.accent,
+        headerTintColor: Color.accent,
         headerTitleStyle: {
             fontWeight: 'bold',
-            fontSize:30
-          }
+            fontSize: 30
+        }
     }
 }
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1
+    safeAreaViewcontainer: {
+        flex: 1,
+    },
+    container: {
+        // flex: 1,
+        backgroundColor: 'yellow'
     }
-  })
+})
 
 export default Dashboard;
